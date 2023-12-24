a49a4cdcc56d8d5086c6e15633900041dc55edfa
KimTalha <dotpy318@gmail.com>
2023-12-24 20:45:06 +0500

    setup postgress database and create a nft model

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
